55b426978fe0c9ae400840a8282dd0dfe51f3cad
nalnguyen <LongNguyen@Long-Nalou.local>
2016-11-01 07:33:59 +0700

    update ref

--- HealthKitExample/HealthManager.swift
+++ HealthKitExample/HealthManager.swift
@@ -13,7 +13,37 @@ class HealthManager {
 
     var healthStore: HKHealthStore?
     
-
+    /*
+     https://developer.apple.com/reference/healthkit
+     https://developer.apple.com/reference/healthkit/1627060-healthkit_constants#//apple_ref/doc/constant_group/Fitness_Identifiers
+     https://developer.apple.com/reference/healthkit/hkcategorytypeidentifier/1615425-sleepanalysis
+     https://developer.apple.com/reference/healthkit/1627060-healthkit_constants/2315150-characteristics_type_identifiers
+     
+     
+     APPCODA
+     https://www.appcoda.com/healthkit-introduction/
+     
+     NATASHA
+     https://www.natashatherobot.com/healthkit-asking-for-identifying-information/
+     https://www.natashatherobot.com/healthkit-units/
+     https://www.natashatherobot.com/healthkit-getting-fitness-data/
+     
+     
+     RAYWENDERLICH
+     https://www.raywenderlich.com/86336/ios-8-healthkit-swift-getting-started
+     
+     
+     RXSWIFT
+     https://viblo.asia/minhtran/posts/XQZkxWOgewA
+     https://chucuoi.net/2016/10/08/gioi-thieu-reactivex-swift/
+     https://kipalog.com/posts/Reactive-Programming-voi-RxSwift
+     https://dangthaison91.wordpress.com/
+     
+     APPWATCH
+     https://dangthaison91.wordpress.com/
+     
+     
+ */
     
     
     func setup() {
